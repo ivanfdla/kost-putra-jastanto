825243b528a08a242c71069213a9f90f3093aec4
ivan <111188262+ivanfdla@users.noreply.github.com>
2026-06-17 17:55:18 +0700

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/kost-putra-jastanto/',
   plugins: [react(), tailwindcss()],
 })
